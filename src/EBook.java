1252b6f99ad65ff4b03ff25725742f72d1b89805
Ouahraniassala <assalaassala866@gmail.com>
2025-05-01 23:10:54 +0100

    Create EBook.java

---
+++ src/EBook.java
@@ -0,0 +1,11 @@
+
+public class EBook extends Book {
+    public EBook(String title, String author, String ISBN) {
+        super(title, author, ISBN);
+    }
+
+    @Override
+    public String getType() {
+        return "إلكتروني";
+    }
+}
